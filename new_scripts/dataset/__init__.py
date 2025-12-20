a9747372663a10745f2450ebf8a1142580a2f2ff
mahdi <mahdipoormirzaie@gmail.com>
2025-12-19 23:43:51 -0600

    add ProteinShake binding site evaluation script and update tokenizer for PDB path resolution

--- new_scripts/dataset/__init__.py
+++ new_scripts/dataset/__init__.py
@@ -1,7 +1,9 @@
 from .biolip2 import BioLIP2FunctionDataset
+from .proteinshake_binding_site import ProteinShakeBindingSiteDataset
 from .tokenizer_biolip2 import WrappedMyRepBioLIP2Tokenizer
 
 __all__ = [
     "BioLIP2FunctionDataset",
+    "ProteinShakeBindingSiteDataset",
     "WrappedMyRepBioLIP2Tokenizer",
 ]
