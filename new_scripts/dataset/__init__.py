e9f1ce41ffd2d5b8246a3c5f4e9161f44af58725
mahdi <mahdipoormirzaie@gmail.com>
2025-12-20 23:07:34 -0600

    add RemoteHomologyDataset and evaluation script for remote homology classification

--- new_scripts/dataset/__init__.py
+++ new_scripts/dataset/__init__.py
@@ -3,6 +3,7 @@ from .biolip2 import BioLIP2FunctionDataset
 from .interpro import InterProFunctionDataset
 from .proteinglue_epitope_region import ProteinGLUEEpitopeRegionDataset
 from .proteinshake_binding_site import ProteinShakeBindingSiteDataset
+from .remote_homology import RemoteHomologyDataset
 from .tokenizer_biolip2 import WrappedMyRepBioLIP2Tokenizer
 
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     "InterProFunctionDataset",
     "ProteinGLUEEpitopeRegionDataset",
     "ProteinShakeBindingSiteDataset",
+    "RemoteHomologyDataset",
     "WrappedMyRepBioLIP2Tokenizer",
 ]
