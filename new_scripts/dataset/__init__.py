5c4238765f491ba432bf528eca094934903c90d9
mahdi <mahdipoormirzaie@gmail.com>
2025-12-20 22:19:03 -0600

    add AtlasDataset and RMSF evaluation script with regression predictor

--- new_scripts/dataset/__init__.py
+++ new_scripts/dataset/__init__.py
@@ -1,3 +1,4 @@
+from .atlas import AtlasDataset
 from .biolip2 import BioLIP2FunctionDataset
 from .interpro import InterProFunctionDataset
 from .proteinglue_epitope_region import ProteinGLUEEpitopeRegionDataset
@@ -5,6 +6,7 @@ from .proteinshake_binding_site import ProteinShakeBindingSiteDataset
 from .tokenizer_biolip2 import WrappedMyRepBioLIP2Tokenizer
 
 __all__ = [
+    "AtlasDataset",
     "BioLIP2FunctionDataset",
     "InterProFunctionDataset",
     "ProteinGLUEEpitopeRegionDataset",
