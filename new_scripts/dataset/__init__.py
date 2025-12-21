a8b9ddbfc0427e24baa23fa8ddda242acdef0e21
mahdi <mahdipoormirzaie@gmail.com>
2025-12-20 22:04:14 -0600

    add ProteinGLUE epitope region dataset and evaluation script with binding predictor

--- new_scripts/dataset/__init__.py
+++ new_scripts/dataset/__init__.py
@@ -1,11 +1,13 @@
 from .biolip2 import BioLIP2FunctionDataset
 from .interpro import InterProFunctionDataset
+from .proteinglue_epitope_region import ProteinGLUEEpitopeRegionDataset
 from .proteinshake_binding_site import ProteinShakeBindingSiteDataset
 from .tokenizer_biolip2 import WrappedMyRepBioLIP2Tokenizer
 
 __all__ = [
     "BioLIP2FunctionDataset",
     "InterProFunctionDataset",
+    "ProteinGLUEEpitopeRegionDataset",
     "ProteinShakeBindingSiteDataset",
     "WrappedMyRepBioLIP2Tokenizer",
 ]
